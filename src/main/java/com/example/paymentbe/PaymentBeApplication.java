41a79f3c8bb9ad24341706a0f64bd4888626861d
Tristan A. <tristanagraa@gmail.com>
2025-03-28 18:22:57 +0700

    chore: init springboot project

---
+++ src/main/java/com/example/paymentbe/PaymentBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.paymentbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/paymentbe/PaymentBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paymentbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
