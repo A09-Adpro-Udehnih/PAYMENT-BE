d8c132a36c37c19c379b7834c72b2e63a44448b4
Tristan A. <tristanagraa@gmail.com>
2025-04-11 10:01:58 +0700

    feat: migration manager

---
+++ src/main/java/com/example/paymentbe/MigrationManager.java
@@ -0,0 +1,13 @@
+package com.example.paymentbe;
+
+import org.flywaydb.core.Flyway;
+
+public class MigrationManager {
+    public static void migrate() {
+        Flyway flyway = Flyway.configure()
+                .dataSource(DatabaseConnection.getDataSource())
+                .locations("classpath:db/migration")
+                .load();
+        flyway.migrate();
+    }
+}
