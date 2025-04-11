566d649e230f1d987ae9ba0e345709baece17a1d
akhyarrasyid <akhyarrasyid2005@gmail.com>
2025-04-11 20:38:23 +0700

    [GREEN] Implement PaymentRepository class

---
+++ src/main/java/com/example/paymentbe/repository/PaymentRepository.java
@@ -0,0 +1,9 @@
+package com.example.paymentbe.repository;
+
+import com.example.paymentbe.model.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface PaymentRepository extends JpaRepository<Payment, UUID> {
+}
