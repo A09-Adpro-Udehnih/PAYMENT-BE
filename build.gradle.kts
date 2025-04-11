79f4367b87a98754a6f3b3f429fcef1a9610b927
akhyarrasyid <akhyarrasyid2005@gmail.com>
2025-04-11 19:12:49 +0700

    chore: config jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
 	java
+	jacoco
 	id("org.springframework.boot") version "3.4.4"
 	id("io.spring.dependency-management") version "1.1.7"
 }
@@ -42,6 +43,14 @@ dependencies {
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-tasks.withType<Test> {
-	useJUnitPlatform()
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
 }
