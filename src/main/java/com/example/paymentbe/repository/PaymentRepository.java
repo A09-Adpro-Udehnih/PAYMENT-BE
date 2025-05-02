bda2c870d6832c0da271170f6b411eff74c4ef7b
akhyarrasyid <akhyarrasyid2005@gmail.com>
2025-05-02 17:41:03 +0700

    feat: added findByUserId and findByStatus implementation in PaymentRepository

--- src/main/java/com/example/paymentbe/repository/PaymentRepository.java
+++ src/main/java/com/example/paymentbe/repository/PaymentRepository.java
@@ -1,9 +1,13 @@
 package com.example.paymentbe.repository;
 
 import com.example.paymentbe.model.Payment;
+import com.example.paymentbe.model.PaymentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface PaymentRepository extends JpaRepository<Payment, UUID> {
+    List<Payment> findByUserId(String userId);
+    List<Payment> findByStatus(PaymentStatus status);
 }
